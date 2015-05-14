899e1ace046e62a5005b4647189112a19695411e
Teemu Tuulari <ttuulari@gmail.com>
2015-05-14 17:09:10 +0300

    Removed test data.

--- src/clj_lspi/lsq.clj
+++ src/clj_lspi/lsq.clj
@@ -4,22 +4,6 @@
     [clojure.core.matrix :refer :all]
     [clojure.core.matrix.operators :refer :all]))
 
-(def training-data 
-  [{:old-state 1 :new-state 4 :action 1 :reward 0}
-   {:old-state 2 :new-state 5 :action 1 :reward 0}
-   {:old-state 3 :new-state 6 :action 2 :reward 1}
-   {:old-state 1 :new-state 4 :action 1 :reward 1}])
-
-(def features
-  [(fn [[state action]]
-     (+ state action))
-   
-   (fn [[state action]]
-     (- action state))
-   
-   (fn [[state action]]
-     1)])
-
 (defn feature-matrix
   "Approximate feature matrix Phi based on data state-actions."
   [features training-data]
